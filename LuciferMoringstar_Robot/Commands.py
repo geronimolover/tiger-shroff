c335cb2322b2608dc96a410b574735c4b6752f7e
vellithiragroup123 <88137446+vellithiragroup123@users.noreply.github.com>
2021-10-23 20:27:03 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -362,9 +362,18 @@ async def bot_info(bot, message):
     
     @Client.on_message(filters.command('help'))
     async def help(bot, message):
+        buttons = [
+        [
+            InlineKeyboardButton
+                (
+                     'Official Group', url="https://t.me/all_super_movies"
+                )
+        ]
+    ]
     await bot.send_photo(
         chat_id=message.from_user.id,
         photo="https://telegra.ph/file/d93aad61f215cf725ff87.jpg",
         caption=f"{HELP}",
         parse_mode="md",
-        reply_to_message_id=message.message_id)
+        reply_to_message_id=message.message_id,
+    reply_markup=InlineKeyboardMarkup(buttons))
