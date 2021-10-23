a6c25e150a0cd885f2403c0e5d673c966a7e96d1
vellithiragroup123 <88137446+vellithiragroup123@users.noreply.github.com>
2021-10-23 10:15:20 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -17,31 +17,16 @@ HELP = """
 ➥ /about : About Me
 ➥ /stats user Status
 
-<b><u>Bot Owner Only</u></b>
-
-➥ /broadcast Replay Any Message or Media
-➥ /ban_user  click ban_user more info
-➥ /unban_user click unban_user more info
-➥ /banned_users Banned User Details
-➥ /total How Many Files Added In Database
-➥ /logger  Get Logs
-➥ /delete Delete File From Database
-
-<b>[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT></b>
 """
 
 ABOUT = """
-➥ Name : Auto Filter With IMDB
+➥ Name : Tiger Shroff
     
-➥ Credits : Subin
-
-➥ Editing : [Muhammed RK](t.me/Mrk_YT)
+➥ Developer : [Geronimo](t.me/Geronimo1234) 
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Source Code : Unavailable
 """
