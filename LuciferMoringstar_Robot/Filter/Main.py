69492757a4906dcf81446b46ed0a29be39f898af
vellithiragroup123 <88137446+vellithiragroup123@users.noreply.github.com>
2021-10-23 13:12:54 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -133,9 +133,8 @@ async def group(client, message):
             LuciferMoringstar=await client.send_photo(
         chat_id=message.chat.id,
         photo="https://telegra.ph/file/86f4da717fc9ba639e469.png",
-        caption="👋Hey {message.from_user.mention}\n👉If this movie is not in our database you will not get that movie..\n\n👉Otherwise, the spelling of the name of the requested movie may not be correct...\nSo you go to google and check the spelling of the name of the movie you want.\n\n👉Check if the movie is released\n\n👉Or wait till the movie uploads",
-        reply_to_message_id=message.message_id,
-        parse_mode="md")
+        caption="👋Hey {message.from_user.mention}\n👉If this movie is not in our database you will not get that movie..\n\n👉Otherwise, the spelling of the name of the requested movie may not be correct...\nSo you go to [Google](https://google.com/) and check the spelling of the name of the movie you want.\n\n👉Check if the movie is released\n\n👉Or wait till the movie uploads",
+        reply_to_message_id=message.message_id)
             return
         if not btn:
             return
